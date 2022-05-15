082b31d6eeec14207a5643830888d4292fd43ae5
Luca Rulvoni <lucarulvoni@gmail.com>
2022-05-15 20:24:09 +0200

    update

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="mt-auto text-white-50">
+    <p>Realizzato da <a href="https://spritecoder.com/" class="text-white">Sprite Coder Software</a>, by <a href="https://twitter.com/spritecoder" class="text-white">@spritecoder</a> </br>Un prodotto software di SAITEK SAS | P.iva IT07900810016.</p>
+</footer>
